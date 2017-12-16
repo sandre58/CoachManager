13200f4882820e2d4a7917ef832a4c9bbd815701
Stéphane ANDRE <andre.cs2i@gmail.com>
2017-12-16 11:38:56 +0100

    Change theme

---
+++ My.CoachManager.Presentation.Prism.Core/Converters/AnyToVisibilityConverter.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Windows;
+using System.Windows.Data;
+
+namespace My.CoachManager.Presentation.Prism.Core.Converters
+{
+    public class AnyToVisibilityConverter : IValueConverter
+    {
+        #region Static Fields
+
+        private static AnyToVisibilityConverter _instance;
+
+        #endregion Static Fields
+
+        #region Public Properties
+
+        /// <summary>
+        /// Return a unique instance of <see cref="AnyToVisibilityConverter"/>.
+        /// </summary>
+        public static AnyToVisibilityConverter Instance
+        {
+            get
+            {
+                return _instance ?? (_instance = new AnyToVisibilityConverter());
+            }
+        }
+
+        #endregion Public Properties
+
+        #region Public Methods and Operators
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var enumerable = (IEnumerable<object>)value;
+            return (value == null || !enumerable.Any()) ? Visibility.Hidden : Visibility.Visible;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion Public Methods and Operators
+    }
+}
